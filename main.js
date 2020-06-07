40f838b61165c0238a3f0b8c190208d85d09a0f9
Moch Fachriyan <mochfachriyan@gmail.com>
2020-06-07 23:50:21 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+generateLink.addEventListener('submit', function(e) {
+    var phoneNumber = document.getElementById('phoneNumber').value;
+    var userMessage = document.getElementById('userMessage').value;
+    var hide = document.getElementById('hide');
+    var generatedLink = 'https://api.whatsapp.com/send?phone=+'+phoneNumber+'&text='+userMessage;
+    var link = document.getElementById('generatedLink');
+    var validate = isNaN(phoneNumber);
+  
+    if (validate == true) {
+  
+      link.innerHTML = "Nomor WhatssApp Invalid";
+      if (hide.className == 'hide') {
+        hide.className = 'show';
+      }
+  
+    }else{
+      link.innerHTML = generatedLink;
+      link.setAttribute('href', generatedLink);
+      if (hide.className == 'hide') {
+        hide.className = 'show';
+      }
+    }
+  e.preventDefault();
+  });
